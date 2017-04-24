f237f3f9d88941780674e9cc0920d7b090293524
iceinmyeye <iceinmyeye@163.com>
2017-04-24 21:00:24 +0800

    修改

---
+++ app/src/main/java/com/mindray/yoursteps/algo/StepCounter.java
@@ -0,0 +1,9 @@
+package com.mindray.yoursteps.algo;
+
+/**
+ * Created by 董小京 on 2017/4/24.
+ * 计步算法的实现
+ */
+
+public class StepCounter {
+}
---
+++ app/src/main/java/com/mindray/yoursteps/view/impl/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.mindray.yoursteps.view.impl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mindray.yoursteps.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
