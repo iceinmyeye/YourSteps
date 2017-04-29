5b15203c5a19129f5c71227de067684800ed55b8
iceinmyeye <iceinmyeye@163.com>
2017-04-29 23:23:48 +0800

    添加：设置界面toolbar

--- app/src/main/java/com/mindray/yoursteps/view/impl/SettingsActivity.java
+++ app/src/main/java/com/mindray/yoursteps/view/impl/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.mindray.yoursteps.view.impl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mindray.yoursteps.R;
 
@@ -11,5 +13,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingsActivity.this.finish();
+            }
+        });
     }
 }
