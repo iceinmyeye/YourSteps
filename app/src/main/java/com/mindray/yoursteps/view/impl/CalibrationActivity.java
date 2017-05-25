118b3df283920d33c5cab7c2a165b93cce622906
iceinmyeye <iceinmyeye@163.com>
2017-05-25 15:43:57 +0800

    初始化校准活动

---
+++ app/src/main/java/com/mindray/yoursteps/view/impl/CalibrationActivity.java
@@ -0,0 +1,15 @@
+package com.mindray.yoursteps.view.impl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mindray.yoursteps.R;
+
+public class CalibrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibration);
+    }
+}
